3a62dbb8d33702909bf1220fe08fddf6190a15aa
javierperezferrada <javier_dc2@hotmail.com>
2014-11-30 17:23:30 -0300

    entrada y salida de mach1 falta agregar al algoritmo el limite de unidades en bodega.

--- simulacion_4.py
+++ simulacion_4.py
@@ -3,7 +3,7 @@ import random
 from math import log
 
 # Constantes
-simulaciones=100
+simulaciones=10000
 
 
 #Variables
@@ -11,6 +11,10 @@ simulaciones=100
 l_rep = [] 			# l_rep es la variable que contiene todos los repuestos que entran al sistema.
 na_llegada = []		# Variable que contendrá los números aleatorios para la estimación del tiempo de llegada de los repuestos. 
 te_llegada = []		# tiempo entre llegadas.
+h_llegada = []		# hora de llegada.
+entrada_mach1 = []	# entrada a la maquina 1
+salida_mach1 = [] 	# salida maquina 1
+t_bodega1 = []		# tiempo en bodega 1
 
 # Llenado de la variable l_rep con datos correlativos desde 1 hasta el numero ingresado de simulaciones.
 # y la variable con los numeros aleatorios para el tiempo entre llegadas de repuestos al sistema.
@@ -23,4 +27,27 @@ for i in range(simulaciones):
 	t = -0.0588 * log(na_llegada[i])
 	te_llegada.append(t)
 
-print te_llegada
+#Hora de llegada
+h_llegada.append(te_llegada[0])
+for i in range(1,simulaciones,1):
+	h = h_llegada[i-1] + te_llegada[i]
+	h_llegada.append(h)
+
+#entrada y salida de la maquina 1 
+# Falta algoritmo para considerar cuando la bodega esta llena.
+entrada_mach1.append(h_llegada[0])
+salida_mach1.append(h_llegada[0]+0.04)
+for i in range(1,simulaciones,1):
+	if h_llegada[i] > salida_mach1[i-1]:
+		entrada_mach1.append(h_llegada[i])
+		salida_mach1.append(h_llegada[i]+0.04)
+	else:
+		entrada_mach1.append(salida_mach1[i-1])
+		salida_mach1.append(salida_mach1[i-1]+0.04)
+
+for i in range(simulaciones):
+	t_bodega1.append(entrada_mach1[i]-h_llegada[i])
+
+print sum(t_bodega1)/simulaciones
+
+
