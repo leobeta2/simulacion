a611b183b27bd079070d5284a3fc08b1d12092bb
leobeta2 <leobeta2@gmail.com>
2014-12-04 00:26:49 -0300

    tiene la mayoria de los procedimientos, falta darles la lógica del tiempo en bodega y del tiempo en reparacion

--- simulacion_4.py
+++ simulacion_4.py
@@ -3,8 +3,8 @@ import random
 from math import log
 
 # Constantes
-simulaciones=10000
-
+#simulaciones =10000
+simulaciones = input('Numero de entradas: ') 
 
 #Variables
 
@@ -36,7 +36,9 @@ for i in range(1,simulaciones,1):
 #entrada y salida de la maquina 1 
 # Falta algoritmo para considerar cuando la bodega esta llena.
 entrada_mach1.append(h_llegada[0])
+#se le suma tiempo trabajado en mach1
 salida_mach1.append(h_llegada[0]+0.04)
+
 for i in range(1,simulaciones,1):
 	if h_llegada[i] > salida_mach1[i-1]:
 		entrada_mach1.append(h_llegada[i])
@@ -45,9 +47,51 @@ for i in range(1,simulaciones,1):
 		entrada_mach1.append(salida_mach1[i-1])
 		salida_mach1.append(salida_mach1[i-1]+0.04)
 
-for i in range(simulaciones):
+for i in range(simulaciones): 
 	t_bodega1.append(entrada_mach1[i]-h_llegada[i])
 
-print sum(t_bodega1)/simulaciones
+print 'tiempo promedio en bodega: ', sum(t_bodega1)/simulaciones #promedio tiempo en bodega primera etapa
 
+#para la segunda etapa
 
+na_llegadaF = []     # Variable aleatoria para las fallas
+tf_llegada = []		# tiempo entre fallas.
+na_reparacion = []
+t_reparacion = []
+entrada_mach2 = []
+salida_mach2 = []
+
+#FALLLAS
+for i in range(simulaciones):
+	l_rep.append(i)
+	na_llegadaF.append(random.random())
+
+for i in range(simulaciones):
+	t = 8+2*(-3+6 * na_llegadaF[i]) 
+	tf_llegada.append(t)
+
+#REPARACION
+for i in range(simulaciones):
+	l_rep.append(i)
+	na_reparacion.append(random.random())
+
+for i in range(simulaciones):
+	t = 1 + 2 * na_reparacion[i]
+	t_reparacion.append(t)
+
+entrada_mach2.append(salida_mach1[0])
+
+salida_mach2.append(salida_mach1[0]+0.04)
+
+#maquina en caso de que no halla falla, por lo tanto tampoco reparacion
+
+for i in range(1,simulaciones,1):
+	if salida_mach1[i] > salida_mach2[i-1]:
+		entrada_mach2.append(salida_mach1[i])
+		salida_mach2.append(salida_mach1[i]+0.0357)
+	else:
+		entrada_mach2.append(salida_mach2[i-1])
+		salida_mach2.append(salida_mach2[i-1]+0.0357)
+
+print 'Tiempo trabajo mach 1: '
+print 'Tiempo trabajo mach 2: '
